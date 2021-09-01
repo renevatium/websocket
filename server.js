7ea0e3244c7aeaf985012c89ec9087cb8bacabaf
Renevatium <rene@developmental.co.za>
2021-09-01 19:06:07 +0100

    updated for aws websockets logic

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 
 'use strict'
 
+// https://github.com/aws-samples/simple-websockets-chat-app
+
 const express = require('express')
 const http = require('http')
 const WebSocket = require('ws')
@@ -9,13 +11,66 @@ const app = express()
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
 
+let clients = {}
+
 wss.on('connection', ws => {
   ws.on('message', message => {
-    wss.clients.forEach(client => {
-      if (client != ws) {
-        client.send(message)
+
+    console.log(message)
+
+    let messageData = JSON.parse(message)
+    let data = messageData.data
+
+    if(data.action == 'joinmeeting') {
+
+      clients[data.fromId] = {
+        id: data.fromId,
+        socket: ws,
+        map: Object.keys(clients).map(id => {
+
+          clients[id].socket.send(JSON.stringify({
+            action: 'requestoffer',
+            fromId: data.fromId
+          }))
+
+          clients[id].map.push({
+            id: data.fromId,
+            type: 'offer'
+          })
+
+          return {
+            id: id,
+            type: 'answer'
+          }
+        })
       }
-    })
+
+    } else if(data.action == 'offer') {
+
+      clients[data.toId].socket.send(JSON.stringify({
+        action: 'receiveoffer',
+        fromId: data.fromId,
+        offer: data.offer
+      }))
+
+    } else if(data.action == 'answer') {
+
+      clients[data.toId].socket.send(JSON.stringify({
+        action: 'receiveanswer',
+        fromId: data.fromId,
+        answer: data.answer
+      }))
+
+    } else if(data.action == 'candidate') {
+
+      clients[data.toId].socket.send(JSON.stringify({
+        action: 'receivecandidate',
+        fromId: data.fromId,
+        answer: data.candidate
+      }))
+
+    }
+
   })
 })
 
