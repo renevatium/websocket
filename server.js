c7a6238af8a78550e544c21b3a84df7dfc05c9d2
Renevatium <rene@developmental.co.za>
2021-10-13 16:32:22 +0100

    recursive stochastic relay

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ let clients = {}
 wss.on('connection', ws => {
   ws.on('message', message => {
 
-    console.log(message)
-
     let messageData = JSON.parse(message)
     let data = messageData.data
 
@@ -26,24 +24,138 @@ wss.on('connection', ws => {
       clients[data.fromId] = {
         id: data.fromId,
         socket: ws,
+        type: 'host',
+        max: data.maxConnections,
         map: Object.keys(clients).map(id => {
 
-          clients[id].socket.send(JSON.stringify({
-            action: 'requestoffer',
-            fromId: data.fromId
-          }))
+          if(clients[id].type == 'host') {
+            clients[id].socket.send(JSON.stringify({
+              action: 'requestoffer',
+              fromId: data.fromId
+            }))
 
-          clients[id].map.push({
-            id: data.fromId,
-            type: 'offer'
-          })
+            clients[id].map.push({
+              id: data.fromId,
+              type: 'host'
+            })
 
-          return {
-            id: id,
-            type: 'answer'
+            return {
+              id: id,
+              type: 'host'
+            }
           }
+
         })
       }
+
+    } else if(data.action == 'joinwebinar') {
+
+      let clientIds = Object.keys(clients)
+      for(let i=0;i<clientIds.length;i++) {
+
+        let toId = clientIds[i]
+        if(clients[toId].type == 'host') {
+
+          if(clients[toId].map.length < clients[toId].max) {
+
+            clients[toId].socket.send(JSON.stringify({
+              action: 'requestoffer',
+              fromId: data.fromId
+            }))
+
+            clients[data.fromId] = {
+              id: data.fromId,
+              socket: ws,
+              type: 'peer',
+              map: [{
+                id: toId,
+                type: 'host'
+              }]
+            }
+
+            clients[toId].map.push({
+              id: data.fromId,
+              toId: toId,
+              type: 'peer',
+              max: data.maxConnections,
+              map: []
+            })
+
+            break
+
+          } else {
+
+            const requestRelay = (peer, depth) => {
+
+              if(peer.map.length < peer.max) {
+
+                clients[peer.id].socket.send(JSON.stringify({
+                  action: 'requestrelay',
+                  fromId: data.fromId,
+                  toId: peer.toId
+                }))
+
+                clients[data.fromId] = {
+                  id: data.fromId,
+                  socket: ws,
+                  type: 'relay',
+                  depth: depth,
+                  map: [{
+                    id: peer.toId,
+                    peerId: peer.id,
+                    type: 'relay'
+                  }]
+                }
+
+                peer.map.push({
+                  id: data.fromId,
+                  toId: peer.id,
+                  relayId: peer.toId,
+                  type: 'relay',
+                  depth: depth,
+                  max: data.maxConnections,
+                  map: []
+                })
+
+                return true
+              }
+
+              return false
+            }
+
+            const requestRelayLoop = (peers, depth) => {
+              let nextLayer = []
+              let relaySuccessful = false
+              if(peers.length > 0) {
+                for(let j=0;j<peers.length;j++) {
+                  relaySuccessful = requestRelay(peers[j], depth)
+                  if(relaySuccessful) {
+                    //console.log(data.fromId, 'relaying', peers[j].toId, 'through', peers[j].id)
+                    return true
+                  } else {
+                    nextLayer = nextLayer.concat(peers[j].map)
+                  }
+                }
+                return requestRelayLoop(nextLayer, depth+1)
+              }
+            }
+
+            requestRelayLoop(clients[toId].map, 1)
+
+          }
+        }
+      }
+
+    } else if(data.action == 'leavemeeting') {
+
+      delete clients[data.fromId]
+      Object.keys(clients).map(id => {
+        clients[id].map.filter(client => client.id != data.fromId);
+        clients[id].socket.send(JSON.stringify({
+          action: 'leavemeeting',
+          fromId: data.fromId
+        }))
+      })
 
     } else if(data.action == 'offer') {
 
@@ -66,8 +178,28 @@ wss.on('connection', ws => {
       clients[data.toId].socket.send(JSON.stringify({
         action: 'receivecandidate',
         fromId: data.fromId,
-        answer: data.candidate
+        candidate: data.candidate
       }))
+
+    } else if(data.action == 'togglevideo') {
+
+      Object.values(clients[data.fromId].map).forEach(peer => {
+        clients[peer.id].socket.send(JSON.stringify({
+          action: 'toggleremotevideo',
+          fromId: data.fromId,
+          state: data.state
+        }))
+      })
+
+    } else if(data.action == 'toggleaudio') {
+
+      Object.values(clients[data.fromId].map).forEach(peer => {
+        clients[peer.id].socket.send(JSON.stringify({
+          action: 'toggleremoteaudio',
+          fromId: data.fromId,
+          state: data.state
+        }))
+      })
 
     }
 
